524c24432ec8d51e159ced1fc27d7b7c98e01536
Anshul upadhyay <Anshulupadhyay544@gmail.com>
2025-06-26 00:03:31 +0530

    Update musicLibrary.py

---
+++ musicLibrary.py
@@ -0,0 +1,10 @@
+music = {
+    "love": "https://youtu.be/yIzCBU0_LyY?si=zYll6sBTVgreetmu",
+    "sky": "https://youtu.be/sXdG1SbRd3A?si=rbaP8ZwMAxaWBmlZ",
+    "break": "https://youtu.be/TFgodv_aaYo?si=LG92iYEgdJ__8u37",
+    "heart": "https://youtu.be/-Px0efU00uQ?si=rbTxtsAXA2wzsszP",
+    "beautiful": "https://youtu.be/g7XhrF-jM74?si=INukf5Eye1mdByGL",
+    "cloud": "https://youtu.be/DeumyOzKqgI?si=pB3kTKqNV6pCFqT9",
+    "hope": "https://youtu.be/tsmPCi7NKrg?si=z9hHdLY1YSqJqFh_",
+    "shreya": "https://youtu.be/jmpUP1MaQ9Q?si=43jzRQ8iOWa2CHpM"
+}
